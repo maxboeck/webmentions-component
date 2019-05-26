bafe636d056c50586e33eb9b478feb2c90925d5c
Max Böck <hello@mxb.at>
2019-05-26 14:23:21 +0200

    basic setup

---
+++ src/web-mentions.js
@@ -0,0 +1,11 @@
+import { LitElement, html } from 'lit-element'
+
+class WebMentions extends LitElement {
+    render() {
+        return html`
+            <div>The Component Template</div>
+        `
+    }
+}
+
+customElements.define('web-mentions', WebMentions)
