7acb96b462b588f7a432ffc9d78fda741af40756
Max Böck <hello@mxb.at>
2019-07-14 16:31:20 +0200

    reformat

--- src/web-mention.js
+++ src/web-mention.js
@@ -1,90 +1,90 @@
 import { LitElement, html, css } from 'lit-element'
 
 class WebMention extends LitElement {
-    constructor() {
-        super()
+  constructor() {
+    super()
 
-        this.url = ''
-        this.author = 'Anonymous'
-        this.avatar = ''
-        this.published = ''
+    this.url = ''
+    this.author = 'Anonymous'
+    this.avatar = ''
+    this.published = ''
+  }
+
+  static get properties() {
+    return {
+      url: { type: String },
+      author: { type: String },
+      avatar: { type: String },
+      published: { type: String }
     }
+  }
 
-    static get properties() {
-        return {
-            url: { type: String },
-            author: { type: String },
-            avatar: { type: String },
-            published: { type: String }
-        }
-    }
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+      img {
+        border-radius: 50%;
+        object-fit: cover;
+        margin-right: 0.5em;
+        background-color: #ededed;
+      }
+      .meta,
+      .meta > a {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+      }
+      .meta {
+        margin-bottom: 0.5em;
+      }
+      .meta > a {
+        margin-right: 1em;
+      }
+      time {
+        font-size: 0.875em;
+      }
+    `
+  }
 
-    static get styles() {
-        return css`
-            :host {
-                display: block;
-            }
-            img {
-                border-radius: 50%;
-                object-fit: cover;
-                margin-right: 0.5em;
-                background-color: #ededed;
-            }
-            header,
-            header > a {
-                display: flex;
-                flex-wrap: wrap;
-                align-items: center;
-            }
-            header {
-                margin-bottom: 0.5em;
-            }
-            header > a {
-                margin-right: 1em;
-            }
-            time {
-                font-size: 0.875em;
-            }
-        `
-    }
+  formatDate(iso) {
+    const date = new Date(iso)
+    const zeroPad = num => (num < 10 ? '0' + num : num)
 
-    formatDate(iso) {
-        const date = new Date(iso)
-        const zeroPad = num => (num < 10 ? '0' + num : num)
+    const dd = zeroPad(date.getDate())
+    const MM = zeroPad(date.getMonth() + 1)
+    const yyyy = date.getFullYear()
+    const HH = zeroPad(date.getHours())
+    const mm = zeroPad(date.getMinutes())
 
-        const dd = zeroPad(date.getDate())
-        const MM = zeroPad(date.getMonth() + 1)
-        const yyyy = date.getFullYear()
-        const HH = zeroPad(date.getHours())
-        const mm = zeroPad(date.getMinutes())
+    return `${dd}.${MM}.${yyyy} - ${HH}:${mm}`
+  }
 
-        return `${dd}.${MM}.${yyyy} - ${HH}:${mm}`
-    }
-
-    render() {
-        return html`
-            <div class="h-cite">
-                <header>
-                    <a href=${this.url} class="u-url">
-                        <img
-                            src=${this.avatar}
-                            class="u-photo"
-                            width="40"
-                            height="40"
-                            alt=""
-                        />
-                        <span class="p-name">${this.author}</span>
-                    </a>
-                    <time datetime=${this.published} class="dt-published">
-                        ${this.formatDate(this.published)}
-                    </time>
-                </header>
-                <div class="p-content">
-                    <slot></slot>
-                </div>
-            </div>
-        `
-    }
+  render() {
+    return html`
+      <div class="h-cite">
+        <div class="meta">
+          <a href=${this.url} class="u-url">
+            <img
+              src=${this.avatar}
+              class="u-photo"
+              width="40"
+              height="40"
+              alt=""
+            />
+            <span class="p-name">${this.author}</span>
+          </a>
+          <time datetime=${this.published} class="dt-published">
+            ${this.formatDate(this.published)}
+          </time>
+        </div>
+        <div class="p-content">
+          <slot></slot>
+        </div>
+      </div>
+    `
+  }
 }
 
 customElements.define('web-mention', WebMention)
--- src/web-mentions.js
+++ src/web-mentions.js
@@ -5,170 +5,170 @@ import { unsafeHTML } from 'lit-html/directives/unsafe-html.js'
 import './web-mention.js'
 
 class WebMentions extends LitElement {
-    constructor() {
-        super()
+  constructor() {
+    super()
 
-        this.webmentions = []
-        this.url = window.location.href
-        this.types = ['in-reply-to', 'mention-of']
-        this.size = 10
-        this.page = 0
-        this.isLastPage = false
+    this.webmentions = []
+    this.url = window.location.href
+    this.types = ['in-reply-to', 'mention-of']
+    this.size = 10
+    this.page = 0
+    this.isLastPage = false
+    this.isLoading = false
+  }
+
+  static get properties() {
+    return {
+      url: { type: String },
+      types: { type: Array },
+      size: { type: Number },
+      page: { type: Number, reflect: true },
+      isLastPage: { type: Boolean, attribute: false },
+      webmentions: { type: Array, attribute: false },
+      isLoading: { type: Boolean, attribute: false }
+    }
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+      ol {
+        padding: 0;
+        margin: 0;
+        list-style-type: none;
+      }
+      li {
+        margin-bottom: 2em;
+      }
+    `
+  }
+
+  connectedCallback() {
+    super.connectedCallback()
+    this.init()
+  }
+
+  buildQueryString(data) {
+    const query = []
+    for (let param in data) {
+      if (data.hasOwnProperty(param)) {
+        const value = data[param]
+        if (Array.isArray(value)) {
+          value.forEach(entry => {
+            query.push(`${param}[]=${encodeURIComponent(entry)}`)
+          })
+        } else {
+          query.push(`${param}=${encodeURIComponent(value)}`)
+        }
+      }
+    }
+    return query.join('&')
+  }
+
+  process(data) {
+    const checkRequiredFields = entry => {
+      const { author, published, content } = entry
+      return !!author && !!author.name && !!published && !!content
+    }
+
+    const sanitizeContent = entry => {
+      const { text, html } = entry.content
+      if (html) {
+        entry.content.value = html
+      } else {
+        entry.content.value = `<p>${text}</p>`
+      }
+      return entry
+    }
+
+    return data
+      .filter(entry => this.types.includes(entry['wm-property']))
+      .filter(checkRequiredFields)
+      .map(sanitizeContent)
+  }
+
+  async init() {
+    this.webmentions = await this.fetchData()
+  }
+
+  async loadNextPage() {
+    this.page += 1
+    const nextPage = await this.fetchData()
+    this.webmentions = this.webmentions.concat(nextPage)
+  }
+
+  async fetchData() {
+    const API_ORIGIN = 'https://webmention.io/api/mentions.jf2'
+    const query = this.buildQueryString({
+      target: this.url,
+      page: this.page,
+      'per-page': this.size
+    })
+
+    try {
+      this.isLoading = true
+      const response = await fetch(`${API_ORIGIN}?${query}`)
+      if (response.ok) {
+        const json = await response.json()
+        const webmentions = this.process(json.children)
+
+        if (json.children.length < this.size) {
+          this.isLastPage = true
+        }
+
         this.isLoading = false
+        return webmentions
+      }
+    } catch (err) {
+      console.error(err)
+      this.isLoading = false
+      return false
+    }
+  }
+
+  render() {
+    if (!this.isLoading && !this.webmentions.length) {
+      return html`
+        <p>No Webmentions yet.</p>
+      `
     }
 
-    static get properties() {
-        return {
-            url: { type: String },
-            types: { type: Array },
-            size: { type: Number },
-            page: { type: Number, reflect: true },
-            isLastPage: { type: Boolean, attribute: false },
-            webmentions: { type: Array, attribute: false },
-            isLoading: { type: Boolean, attribute: false }
-        }
-    }
+    const loadingMsg = html`
+      <p>Loading Webmentions...</p>
+    `
 
-    static get styles() {
-        return css`
-            :host {
-                display: block;
-            }
-            ol {
-                padding: 0;
-                margin: 0;
-                list-style-type: none;
-            }
-            li {
-                margin-bottom: 2em;
-            }
+    const nextPageBtn = !this.isLastPage
+      ? html`
+          <button type="button" @click=${this.loadNextPage}>
+            Load Next Page
+          </button>
         `
-    }
+      : null
 
-    connectedCallback() {
-        super.connectedCallback()
-        this.init()
-    }
-
-    buildQueryString(data) {
-        const query = []
-        for (let param in data) {
-            if (data.hasOwnProperty(param)) {
-                const value = data[param]
-                if (Array.isArray(value)) {
-                    value.forEach(entry => {
-                        query.push(`${param}[]=${encodeURIComponent(entry)}`)
-                    })
-                } else {
-                    query.push(`${param}=${encodeURIComponent(value)}`)
-                }
-            }
-        }
-        return query.join('&')
-    }
-
-    process(data) {
-        const checkRequiredFields = entry => {
-            const { author, published, content } = entry
-            return !!author && !!author.name && !!published && !!content
-        }
-
-        const sanitizeContent = entry => {
-            const { text, html } = entry.content
-            if (html) {
-                entry.content.value = html
-            } else {
-                entry.content.value = `<p>${text}</p>`
-            }
-            return entry
-        }
-
-        return data
-            .filter(entry => this.types.includes(entry['wm-property']))
-            .filter(checkRequiredFields)
-            .map(sanitizeContent)
-    }
-
-    async init() {
-        this.webmentions = await this.fetchData()
-    }
-
-    async loadNextPage() {
-        this.page += 1
-        const nextPage = await this.fetchData()
-        this.webmentions = this.webmentions.concat(nextPage)
-    }
-
-    async fetchData() {
-        const API_ORIGIN = 'https://webmention.io/api/mentions.jf2'
-        const query = this.buildQueryString({
-            target: this.url,
-            page: this.page,
-            'per-page': this.size
-        })
-
-        try {
-            this.isLoading = true
-            const response = await fetch(`${API_ORIGIN}?${query}`)
-            if (response.ok) {
-                const json = await response.json()
-                const webmentions = this.process(json.children)
-
-                if (json.children.length < this.size) {
-                    this.isLastPage = true
-                }
-
-                this.isLoading = false
-                return webmentions
-            }
-        } catch (err) {
-            console.error(err)
-            this.isLoading = false
-            return false
-        }
-    }
-
-    render() {
-        if (!this.isLoading && !this.webmentions.length) {
-            return html`
-                <p>No Webmentions yet.</p>
-            `
-        }
-
-        const loadingMsg = html`
-            <p>Loading Webmentions...</p>
-        `
-
-        const nextPageBtn = !this.isLastPage
-            ? html`
-                  <button type="button" @click=${this.loadNextPage}>
-                      Load Next Page
-                  </button>
-              `
-            : null
-
-        return html`
-            <ol>
-                ${repeat(
-                    this.webmentions,
-                    item => item['wm-id'],
-                    item => html`
-                        <li>
-                            <web-mention
-                                url=${item.url}
-                                author=${item.author.name}
-                                avatar=${item.author.photo}
-                                published=${item.published}
-                            >
-                                ${unsafeHTML(item.content.value)}
-                            </web-mention>
-                        </li>
-                    `
-                )}
-            </ol>
-            ${this.isLoading ? loadingMsg : nextPageBtn}
-        `
-    }
+    return html`
+      <ol>
+        ${repeat(
+          this.webmentions,
+          item => item['wm-id'],
+          item => html`
+            <li>
+              <web-mention
+                url=${item.url}
+                author=${item.author.name}
+                avatar=${item.author.photo}
+                published=${item.published}
+              >
+                ${unsafeHTML(item.content.value)}
+              </web-mention>
+            </li>
+          `
+        )}
+      </ol>
+      ${this.isLoading ? loadingMsg : nextPageBtn}
+    `
+  }
 }
 
 customElements.define('web-mentions', WebMentions)
